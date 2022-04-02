8321c971828772ead27bec000fef8268d0a0c1a6
manuelabordad <manuelaborda2019@gmail.com>
2022-04-01 19:20:17 -0500

    error encontrando carpeta

--- app.js
+++ app.js
@@ -7,15 +7,20 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+// app.get("/", (req, res) => {
+// 	res.sendFile(`${__dirname}/public/js/index.html`);
+// });
+server.listen(8080, () => {
+	console.log("listeing server on  http://localhost:8080 ");
+});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
 io.on("connection", (socket) => {
 	console.log("new user connected");
 	socket.on("mensajes", (msg) => {
 		console.log("mensaje" + msg);
 	});
-});
-app.get("/", (req, res) => {
-	res.sendFile(`${__dirname}/public/js/index.html`);
-});
-server.listen(3000, () => {
-	console.log("listeing server on  http://localhost:3000 ");
 });
